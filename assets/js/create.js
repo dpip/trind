a3596ca2fb336ee2a0b5afbeaa7823a3332c4f4b
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-06 18:05:13 -0400

    post attempts

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,14 +1,61 @@
-trind.controller('CreateController', [ '$http', '$scope', function($http, $scope){
+trind.controller('CreateController', ['$scope', '$http', function($scope, $http){
 
-    $scope.message = "get was got"
+  console.log("WHY");
+
+  $scope.testTrind = {
+      "events": {
+      "description": ""
+    }
+  };
+
+  $scope.submitTrind = function() {
+    console.log("Hello, little Hobbit!");
+    console.log($scope.testTrind)
+    $http.post('https://still-waters-14036.herokuapp.com/events', $scope.testTrind)
+    .then(function(response){
+      console.log("FUCK");
+    })
+  };
+
+      // {
+      //       "description": $(".user-creator-description-field").val("") })
+
+
+
+
 
     $(document).ready(function () {
         $("#home-drop-search-input-box").hide();
         $('.trind-main-header-search-icon').click(function () {
             $("#home-drop-search-input-box").toggle();
         });
-
     });
+    // $(".create-trind-submit-button").click(function(){
+    //       newEvent = {
+    //         "description": $(".user-creator-description-field").val("")
+    //       }
+    //       console.log(newEvent);
+      //
+      // var newEvent;
 
+      // $.post('https://still-waters-14036.herokuapp.com/events', newEvent).success(function(){
+      //   console.log("CHICKEN DINNER");
+      // })
+      // .error(function(){
+      //   console.log("dammit");
+      // })
+      //       });
+
+      // $(".create-trind-submit-button-text").click(function(){
+      //
+      // $.post('https://still-waters-14036.herokuapp.com/events', trindPLZ).then(console.log("FUCK YES"));
+      //
+      // var trindPLZ = {
+      // "event": {
+      // "description": $(".user-creator-description-field").val("")
+      // }
+      // };
+      //
+      // });
 
   }]);
