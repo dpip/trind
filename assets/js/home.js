622a4db07c297bf7ccae7da11855ef10ca863ed1
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-06 22:54:24 -0400

    yeah

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,6 @@
 trind.controller('HomeController', [ '$http', '$scope', function($http, $scope){
 
-  $http.get('https://still-waters-14036.herokuapp.com/users').success(function(data){
+  $http.get('https://still-waters-14036.herokuapp.com/events').success(function(data){
 
     console.log(data);
     $scope.data = data;
