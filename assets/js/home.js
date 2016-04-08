347e2ef96b8da0a53f37c2b7234e2bad7898bea2
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-08 13:45:39 -0400

    details partial / work done on ng-include

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,31 @@
 trind.controller('HomeController', [ '$http', '$scope', function($http, $scope){
 
+      // Gets the event json from Damian and repeats through home page
       $http.get('https://still-waters-14036.herokuapp.com/events').success(function(data){
-
         console.log(data);
-        $scope.data = data;
-
+        $scope.stuff = data;
     });
 
+      // restrict E... The directive can appear only as HTML element.
+    var controllers = {};
+              $scope.GetDetailView = function ($scope) {
+                  trind.directive('detailsDirective', function () {
+                      return {
+                          restrict: 'E',
+                          templateUrl: '/details'
+                      }
+                      console.log(close);
+                  });
+              };
+
+          controllers.DetailsController = function ($scope) {
+
+          };
+
+
+
+
+      // Drops search bar on click.... would love this functionality to happen on scroll
     $(document).ready(function () {
         $("#home-drop-search-input-box").hide();
         $('.trind-main-header-search-icon').click(function () {
