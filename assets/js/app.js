519d6461425f86c1daca9727a9049f61004221ae
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-10 18:45:55 -0400

    holllllllllllly fb oath

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
 var trind = angular.module('trind', ['ngRoute']);
 
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'your-app-id',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  FB.init({
+    appId  : '485587434980626',
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml  : true  // parse XFBML
+  });
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
 trind.config(function($routeProvider) {
   $routeProvider
     .when('/land', {
