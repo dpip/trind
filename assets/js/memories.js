b9ef21bded2b8f91eb76007195ce120e7da31166
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-10 15:03:50 -0400

    users can rate with thumbs up, memories display styled, me page has scope connected displaying user info, beggining work on token based api requests

--- assets/js/memories.js
+++ assets/js/memories.js
@@ -1,11 +1,28 @@
 trind.controller('MemoriesController', [ '$http', '$scope', function($http, $scope){
 
-  $(document).ready(function () {
-      $("#home-drop-search-input-box").hide();
-      $('.trind-main-header-search-icon').click(function () {
-          $("#home-drop-search-input-box").toggle();
-      });
 
-  });
+    var hasLiked = false;
+    $scope.likeClick = function () {
+        if (!hasLiked) {
+            hasLiked = true;
+            $scope.liked = 'cool';
+            $scope.likeCount += 1;
+        }
+        else {
+            hasLiked = false;
+            $scope.liked = 'Like';
+            $scope.likeCount -= 1;
+        }
+    };
+
+
+
+  // $(document).ready(function () {
+  //     $("#home-drop-search-input-box").hide();
+  //     $('.trind-main-header-search-icon').click(function () {
+  //         $("#home-drop-search-input-box").toggle();
+  //     });
+  //
+  // });
 
   }]);
