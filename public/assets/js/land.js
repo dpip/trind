c7fdfbf5ba2a4156f5a4346f8dcf6c0a1a8ec2e5
Daniel Pipkin <dpip.nc@gmail.com>
2016-04-11 20:38:40 -0400

    mainly under the hood of jand.js and app.js/ temporarily removed jumbotron to view buttons/ recieved fb oath access token

--- public/assets/js/land.js
+++ public/assets/js/land.js
@@ -1,4 +1,4 @@
-trind.controller('LandController', [ '$http', '$scope', function($http, $scope){
+trind.controller('LandController', [ '$http', '$location', '$scope', function($http, $location, $scope){
 
 
 
@@ -21,6 +21,9 @@ trind.controller('LandController', [ '$http', '$scope', function($http, $scope){
 
   };
 
+
+// ++++++++++++++++++email login ++++++++++++++++++++++
+
   $scope.submitLogin = function() {
     console.log("Hello, little Hobbit!");
     console.log($scope.landPage)
@@ -34,30 +37,80 @@ trind.controller('LandController', [ '$http', '$scope', function($http, $scope){
         });
   };
 
-$scope.submitSignup = function() {
-    console.log("Time to get Trinding");
-    console.log($scope.newAccount)
-    $http.post('https://still-waters-14036.herokuapp.com/users', $scope.newAccount)
-    .success(function(data) {
-        console.log("new account created");
-        window.location.replace('#/home');
-    })
+// +++++++++++++++++++sign in email+++++++++++++++++++++
 
-};
-
-$scope.FBlogin = function() {
-  FB.login(function(response) {
-  if (response.authResponse) {
-   console.log('Welcome!  Fetching your information.... ');
-   FB.api('/me', function(response) {
-     console.log('Good to see you, ' + response.name + '.');
-   });
-  } else {
-   console.log('User cancelled login or did not fully authorize.');
-  }
-});
-};
+  $scope.submitSignup = function() {
+      console.log("Time to get Trinding");
+      console.log($scope.newAccount)
+      $http.post('https://still-waters-14036.herokuapp.com/users', $scope.newAccount)
+      .success(function(data) {
+          console.log("new account created");
+          window.location.replace('#/home');
+      })
+  };
 
 
+  $scope.goHome = function() {
+      console.log(go);
+      $location.url('#/home')
+    };
 
-  }]);
+
+// +++++++++++++++++initial communication with facebook+++++++++++++++++++++++
+function statusChangeCallback(response) {
+console.log('statusChangeCallback');
+console.log(response);
+// The response object is returned with a status field that lets the
+// app know the current login status of the person.
+// Full docs on the response object can be found in the documentation
+// for FB.getLoginStatus().
+if (response.status === 'connected') {
+  // Logged into your app and Facebook.
+  testAPI();
+} else if (response.status === 'not_authorized') {
+  // The person is logged into Facebook, but not your app.
+  document.getElementById('status').innerHTML = 'Please log ' +
+    'into this app.';
+} else {
+  // The person is not logged into Facebook, so we're not sure if
+  // they are logged into this app or not.
+  document.getElementById('status').innerHTML = 'Please log ' +
+    'into Facebook.';
+}
+}
+
+
+  $scope.FBlogin = function() {
+    FB.login(function(response) {
+    if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log(response);
+     });
+    } else {
+     console.log('User cancelled login or did not fully authorize.');
+    }
+  });
+  };
+
+    FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  // FB.logout(function(response) {
+  //
+  // });
+// FB.api('/me', function(response) {
+//     console.log(JSON.stringify(response));
+// });
+
+// +++++++++++++++get user token aka post to damian / login ++++++++++++++++++
+
+
+
+
+
+
+
+
+}]);
