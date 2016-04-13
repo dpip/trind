7c6374366f6a9878fb0a26559aa34b36d7e4a105
ebonertz <bonertz.unc@gmail.com>
2016-04-13 15:59:27 -0400

    Dev it Up

--- public/assets/js/me.js
+++ public/assets/js/me.js
@@ -1,11 +1,31 @@
 trind.controller('MeController', [ '$http', '$scope', function($http, $scope){
 
-  $http.get('https://still-waters-14036.herokuapp.com/users').success(function(user){
-    console.log(user);
-    $scope.userInfo = user;
+    var userID = localStorage.getItem('userID');
+    var currentToken = localStorage.getItem('tokenToken');
+
+    $scope.userID = userID;
+    $scope.userName = " "
+    $scope.userEmail = " ";
+
+        $scope.currentUser = {
+            "user_id": {
+            "user_id": userID,
+            "interests": "",
+            "description": "",
+            "location": "",
+            "token": currentToken
+          }
+        };
+
+
+  $http.get('https://still-waters-14036.herokuapp.com/users/' + userID + "?token=" + currentToken).success(function(userInfo){
+    console.log(userID);
+    console.log(currentToken);
+    console.log(userInfo);
+
+    $scope.userName = userInfo.name;
+    $scope.userEmail = userInfo.email;
+
   });
-
-  // =========two gets will screw with things! be careful=========
-
 
   }]);
