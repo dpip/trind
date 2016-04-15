b4d615fb7fd0afc057b7c68aff700d1a1ba6df42
damianhouse <damianhouse@gmail.com>
2016-04-15 14:41:04 -0400

    Details page shows the details of the event.

--- public/assets/js/memories.js
+++ public/assets/js/memories.js
@@ -1,29 +1,6 @@
 trind.controller('MemoriesController', [ '$http', '$scope', function($http, $scope){
 
 
-
-
-  var currentToken = localStorage.getItem('tokenToken');
-  var userID = localStorage.getItem('userID');
-  // var eventID = localStorage.getItem('eventID');
-  var theEventID = localStorage.getItem('theEventID');
-  console.log(theEventID);
-
-
-
-  $http.get('https://still-waters-14036.herokuapp.com/memories/?token=' + currentToken).then(function successCallback(response){
-    console.log('memories', response);
-      $scope.yourMemories = response.data;
-      console.log($scope.yourMemories);
-
-
-  });
-
-
-
-
-
-
     var hasLiked = false;
     $scope.likeClick = function () {
         if (!hasLiked) {
@@ -37,14 +14,31 @@ trind.controller('MemoriesController', [ '$http', '$scope', function($http, $sco
             $scope.likeCount -= 1;
         }
     };
+    
+    $http.get('https://still-waters-14036.herokuapp.com/memories?token=' + currentToken ).success(function(data){
+      console.log(data);
+      $scope.stuff = data;
+        // $scope.eventIDGrab = data.id
+        $scope.details = function(thingId) {
+          console.log("ya there?");
+          showID = thingId;
+          console.log(showID);
+
+          localStorage.setItem('theEventID', showID);
+
+          $location.path('/details');
+        };
+
+        $scope.getEvents = function(event) {
+          console.log("we got there");
+          // localStorage.setItem('theEventID', .id );
+        };
 
 
-    $(document).ready(function () {
-        $("#home-drop-search-input-box").hide();
-        $('.trind-main-header-search-icon').click(function () {
-            $("#home-drop-search-input-box").toggle();
-        });
+        // scope displayind details of user!!!!!!
+        $scope.eventDetail = data.events;
 
-    });
 
-  }]);
+      });
+
+}]);
