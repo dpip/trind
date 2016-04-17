87f8b5b40f7be4f59f66c4ed98e0b044e3a886bb
ebonertz <bonertz.unc@gmail.com>
2016-04-17 16:10:22 -0400

    Ready to roll

--- public/assets/js/details.js
+++ public/assets/js/details.js
@@ -4,7 +4,7 @@ trind.controller('DetailsController', [ '$http', '$location', '$scope', function
 
       var currentToken = localStorage.getItem('tokenToken');
       var userID = localStorage.getItem('userID');
-      // var eventID = localStorage.getItem('eventID');
+      var conversationID = localStorage.getItem('conversationID');
       var theEventID = localStorage.getItem('theEventID');
       console.log(theEventID);
 
@@ -13,18 +13,18 @@ trind.controller('DetailsController', [ '$http', '$location', '$scope', function
         $("#home-drop-search-input-box").toggle();
     });
 
-
       $http.get('https://still-waters-14036.herokuapp.com/events/' + theEventID + "?token=" + currentToken).then(function successCallback(response){
 
           $scope.event = response.data;
           console.log($scope.event);
 
-          $scope.interested = function() {
-            var param = {event:{searcherinterested:userID}}
+          $scope.stash = function() {
+            var param = {event:{searcherinterested:userID}};
+            console.log(param);
 
           $http.put('https://still-waters-14036.herokuapp.com/events/' + theEventID + "?token=" + currentToken, param).then(function successCallback(response){
             console.log(response);
-            $location.path('/memories');
+            // $location.path('/memories');
         });
         },
         function errorCallback(response){
@@ -33,11 +33,9 @@ trind.controller('DetailsController', [ '$http', '$location', '$scope', function
 
       $scope.messagenow = function() {
           console.log("wurd");
-
-        //   $location.path('/message');
-
-      }
-
+          localStorage.setItem('conversationID', conversationID);
+          $location.path('/message');
+      };
 
       });
 }]);
