61cfc61b8adc59b0b276f2925d4806af34ae5202
damianhouse <damianhouse@gmail.com>
2016-04-17 19:18:41 -0400

    Fix user name at the top when clicking into messaging from event.

--- public/assets/js/details.js
+++ public/assets/js/details.js
@@ -6,8 +6,11 @@ trind.controller('DetailsController', [ '$http', '$location', '$scope', function
       var userID = localStorage.getItem('userID');
       var conversationID = localStorage.getItem('conversationID');
       var theEventID = localStorage.getItem('theEventID');
-      console.log(theEventID);
+      var otherUserID = localStorage.getItem('otherUserID');
+      var otherUserName = localStorage.getItem('otherUserName');
 
+      console.log(theEventID);
+      console.log(otherUserID);
       $("#home-drop-search-input-box").hide();
       $('.trind-main-header-search-icon').click(function () {
         $("#home-drop-search-input-box").toggle();
@@ -16,17 +19,34 @@ trind.controller('DetailsController', [ '$http', '$location', '$scope', function
       $http.get('https://still-waters-14036.herokuapp.com/events/' + theEventID + "?token=" + currentToken).then(function successCallback(response){
 
         $scope.event = response.data;
+        $scope.whatIWant = response.data.event.user_id;
+        var otherUserID = response.data.event.user_id;
+        var otherUserName = response.data.user.name;
         console.log($scope.event);
+        console.log($scope.whatIWant);
+        // $scope.whatIWant = localStorage.setItem('otherUserID', otherUserID);
+        localStorage.setItem('otherUserID', otherUserID);
+        localStorage.setItem('otherUserName', otherUserName);
 
         function errorCallback(response){
           console.log('hate', response)
         };
       });
 
-      $scope.messagenow = function() {
+      $scope.messageNow = function() {
           console.log("wurd");
-          localStorage.setItem('conversationID', conversationID);
-          $location.path('/message');
+
+          var param = {conversation:{event_id: theEventID, recipient_id: otherUserID, sender_id: userID}};
+          console.log(param);
+
+          $http.post('https://still-waters-14036.herokuapp.com/conversations?token=' + currentToken, param).success(function (response) {
+            console.log(response);
+            console.log(response.id)
+            var conversationID = response.id;
+            localStorage.setItem('conversationID', conversationID);
+            $location.path('/message');
+
+          })
       };
 
       $scope.stash = function() {
