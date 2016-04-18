fc6d448d2427a39ae7c0c2dfa28952e5163c1d8f
damianhouse <damianhouse@gmail.com>
2016-04-17 23:01:12 -0400

    Clean up code formatting with indentation.

--- public/assets/js/memories.js
+++ public/assets/js/memories.js
@@ -1,33 +1,29 @@
 trind.controller('MemoriesController', [ '$http', '$scope', function($http, $scope){
 
     $(document).ready(function () {
-            $("#home-drop-search-input-box").hide();
-            $('.trind-main-header-search-icon').click(function () {
-                $("#home-drop-search-input-box").toggle();
+        $("#home-drop-search-input-box").hide();
+        $('.trind-main-header-search-icon').click(function () {
+            $("#home-drop-search-input-box").toggle();
+        });
+
+
+        var currentToken = localStorage.getItem('tokenToken');
+        var userID = localStorage.getItem('userID');
+        // var eventID = localStorage.getItem('eventID');
+        var theEventID = localStorage.getItem('theEventID');
+        console.log(theEventID);
+
+
+        $http.get('https://still-waters-14036.herokuapp.com/stashed/?token=' + currentToken).then(function successCallback(response){
+          console.log('memories', response);
+            $scope.yourMemories = response.data;
+            console.log($scope.yourMemories);
+
+        });
     });
 
-
-  var currentToken = localStorage.getItem('tokenToken');
-  var userID = localStorage.getItem('userID');
-  // var eventID = localStorage.getItem('eventID');
-  var theEventID = localStorage.getItem('theEventID');
-  console.log(theEventID);
-
-
-  $http.get('https://still-waters-14036.herokuapp.com/memories/?token=' + currentToken).then(function successCallback(response){
-    console.log('memories', response);
-      $scope.yourMemories = response.data;
-      console.log($scope.yourMemories);
-
-  });
-  });
-
-
-
-
-
-
     var hasLiked = false;
+
     $scope.likeClick = function () {
 
       var param = {user:{rating:$(".rating-test").val()}}
