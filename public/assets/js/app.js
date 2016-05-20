c999da05fda55e12df506dcf973e44f19499580e
Daniel Pipkin <dpip.nc@gmail.com>
2016-05-20 12:49:07 -0400

    minor scss / messages append most current to top

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,18 @@
 "use strict";
 
+// 
+// var express = require("express");
+// var app = express();
+// var port = 3700;
+//
+// app.get("/", function(req, res){
+//     res.send("It works!");
+// });
+//
+// app.listen(port);
+// console.log("Listening on port " + port);
+
+
 var trind = angular.module('trind', ['ngRoute', 'ngFileUpload']);
 
 
